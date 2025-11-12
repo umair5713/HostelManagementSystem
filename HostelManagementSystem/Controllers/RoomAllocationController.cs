87877b868637275b5d07d61d9064bedc6df5138d
wk_khan <waheeba.khan.wk@gmail.com>
2025-11-12 19:14:19 +0500

    room allocation

---
+++ HostelManagementSystem/Controllers/RoomAllocationController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HostelManagementSystem.Models;
+
+namespace HostelManagementSystem.Controllers
+{
+    public class RoomAllocationController : Controller
+    {
+        // One shared queue for the whole system
+        public static RoomQueue queue = new RoomQueue();
+
+        public IActionResult QueueList()
+        {
+            ViewBag.AllStudents = StudentController.students.GetStudents();
+            var waitingStudents = queue.get_queue();
+            return View("~/Views/RoomAllocation/QueueList.cshtml", waitingStudents);
+        }
+
+        [HttpPost]
+        public IActionResult AddToQueue(Student student)
+        {
+            queue.enqueue(student);
+            return RedirectToAction("QueueList");
+        }
+
+        public IActionResult AllocateRoom()
+        {
+            var student = queue.dequeue();
+
+            // If queue was empty
+            if (student == null)
+            {
+                return View("Allocated", null);
+            }
+
+            // Show allocated student result page
+            return View("Allocated", student);
+        }
+
+    }
+}
+
---
+++ HostelManagementSystem/Models/RoomQueue.cs
@@ -0,0 +1,58 @@
+﻿namespace HostelManagementSystem.Models
+{
+    public class RoomQueue
+    {
+        private RoomQueueNode front;
+        private RoomQueueNode back;
+
+        public void enqueue(Student student)
+        {
+            RoomQueueNode newnode = new RoomQueueNode { Data = student, next = null };
+
+            if (front == null)
+            {
+                front = back = newnode;
+            }
+            else
+            {
+                back.next = newnode;
+                back = newnode;
+            }
+        }
+
+        public Student dequeue()
+        {
+            if (front == null)
+            {
+                return null;
+            }
+
+            Student removedStudent = front.Data;
+            front = front.next;
+
+            if (front == null)
+                back = null;   // queue is now empty
+
+            return removedStudent;
+        }
+
+        public bool empty()
+        {
+            return front == null;
+        }
+
+        public List<Student> get_queue()
+        {
+            List<Student> list = new List<Student>();
+            RoomQueueNode temp = front;
+
+            while (temp != null)
+            {
+                list.Add(temp.Data);
+                temp = temp.next;
+            }
+            return list;
+        }
+    }
+}
+
