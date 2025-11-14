79877d9eb21715ef42bfb311d9fb20c6b7775181
umair5713 <umairatique6346@gmail.com>
2025-11-14 20:36:55 +0500

    admin dashboard

--- HostelManagementSystem/Models/User.cs
+++ HostelManagementSystem/Models/User.cs
@@ -1,10 +1,17 @@
-﻿namespace HostelManagementSystem.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HostelManagementSystem.Models
 {
+    [Table("tbl_user")]
     public class User
-    {   
+    {
+        [Column("id_user")]
         public int Id { get; set; }
+        [Column("email")]
         public string Email { get; set; }=string.Empty;
+        [Column("user_password")]
         public string Password { get; set; } = string.Empty;
+        [Column("fk_role_name")]
 
         public string FkRoleName { get; set; } = string.Empty;
     }
