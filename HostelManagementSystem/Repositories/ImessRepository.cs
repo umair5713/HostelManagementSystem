3c72ed14946cffceab65370d169776e395d69855
adina tariq <adinatariq83@gmail.com>
2025-11-15 17:53:12 -0800

    updatedd

---
+++ HostelManagementSystem/Repositories/ImessRepository.cs
@@ -0,0 +1,19 @@
+﻿ using HostelManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+
+namespace HostelManagementSystem.Repositories
+    {
+        // Interface for Mess Repository
+        public interface IMessRepository
+        {
+            List<Student> GetAllStudents();
+            List<MenuItem> GetWeeklyMenu();
+            List<MessAttendance> GetAttendanceByDate(DateTime date);
+            List<MessBilling> GetAllBillings();
+            void AddAttendance(MessAttendance attendance);
+            void UpdateBilling(MessBilling billing);
+        }
+    }
+
+}
