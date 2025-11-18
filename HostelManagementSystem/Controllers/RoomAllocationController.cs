8e2b9abd28bb353152913ad7ef5a5d0d3a6ba036
adina tariq <adinatariq83@gmail.com>
2025-11-17 19:02:44 -0800

    updated view

--- HostelManagementSystem/Controllers/RoomAllocationController.cs
+++ HostelManagementSystem/Controllers/RoomAllocationController.cs
@@ -1,4 +1,5 @@
-﻿using HostelManagementSystem.Models;
+﻿/*
+ using HostelManagementSystem.Models;
 using HostelManagementSystem.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -53,3 +54,73 @@ namespace HostelManagementSystem.Controllers
         }
     }
 }
+*/
+using HostelManagementSystem.Models;
+using HostelManagementSystem.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HostelManagementSystem.Controllers
+{
+    public class RoomAllocationController : Controller
+    {
+        private readonly IRoomAllocationService _service;
+        private readonly IStudentService _studentService;
+
+        public RoomAllocationController(
+            IRoomAllocationService service,
+            IStudentService studentService)
+        {
+            _service = service;
+            _studentService = studentService;
+        }
+
+        // ------------------ NEW INDEX ACTION ------------------
+        // This makes /RoomAllocation/Index open QueueList automatically
+        public IActionResult Index()
+        {
+            return RedirectToAction("QueueList");
+        }
+
+        // ------------------ YOUR MAIN PAGE ------------------
+        // These routes all show the QueueList page
+        [Route("RoomAllocation/QueueList")]
+        [Route("RoomAllocation/AddRoom")]
+        [Route("RoomAllocation/Index")]
+        public IActionResult QueueList()
+        {
+            var allStudents = _studentService.GetAllStudents();
+            var waiting = _service.GetQueue();
+
+            ViewBag.AllStudents = allStudents;
+            return View("~/Views/RoomAllocation/QueueList.cshtml", waiting);
+        }
+
+        [HttpPost]
+        public IActionResult AddToQueue(Student student)
+        {
+            if (student == null)
+            {
+                TempData["Error"] = "Invalid student information.";
+                return RedirectToAction("QueueList");
+            }
+
+            _service.AddToQueue(student);
+            return RedirectToAction("QueueList");
+        }
+
+        public IActionResult AllocateRoom()
+        {
+            var student = _service.AllocateRoom();
+
+            if (student == null)
+            {
+                TempData["Message"] = "No students in queue.";
+                return View("~/Views/RoomAllocation/Allocated.cshtml", null);
+            }
+
+            return View("~/Views/RoomAllocation/Allocated.cshtml", student);
+        }
+    }
+}
+
+
