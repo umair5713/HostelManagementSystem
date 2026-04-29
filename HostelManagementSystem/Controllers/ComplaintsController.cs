566f8704b8c9396308a87623c21fe18e5f137c82
Saad Khan <saad030229@gmail.com>
2026-04-29 15:52:21 +0500

    complain issue resolve

--- HostelManagementSystem/Controllers/ComplaintsController.cs
+++ HostelManagementSystem/Controllers/ComplaintsController.cs
@@ -1,4 +1,92 @@
-﻿using HostelManagementSystem.Models;
+﻿//using HostelManagementSystem.Models;
+//using HostelManagementSystem.Services;
+//using Microsoft.AspNetCore.Mvc;
+
+//namespace HostelManagementSystem.Controllers
+//{
+//    public class ComplaintsController : Controller
+//    {
+//        private readonly IComplaintService _service;
+
+//        public ComplaintsController(IComplaintService service)
+//        {
+//            _service = service;
+//        }
+
+
+//        public IActionResult Index()
+//        {
+//            var complaints = _service.GetAllComplaints();
+//            return View("~/Views/Complaints/Index.cshtml", complaints);
+//        }
+
+//        public IActionResult Admin()
+//        {
+//            var complaints = _service.GetAllComplaints();
+//            return View("~/Views/Complaints/Admin.cshtml", complaints);
+//        }
+
+//        // ADMIN: Update status to "Received"
+//        [HttpPost]
+//        public IActionResult MarkReceived(int id)
+//        {
+//            _service.UpdateComplaintStatus(id, "Received");
+//            return RedirectToAction("Admin");
+//        }
+
+//        // ADMIN: Update status to "In Progress"
+//        [HttpPost]
+//        public IActionResult MarkInProgress(int id)
+//        {
+//            _service.UpdateComplaintStatus(id, "In Progress");
+//            return RedirectToAction("Admin");
+//        }
+
+//        // ADMIN: Update status to "Done"
+//        [HttpPost]
+//        public IActionResult MarkDone(int id)
+//        {
+//            _service.UpdateComplaintStatus(id, "Done");
+//            return RedirectToAction("Admin");
+//        }
+
+//        // ADMIN: Delete complaint
+//        [HttpPost]
+//        public IActionResult Delete(int id)
+//        {
+//            _service.DeleteComplaint(id);
+//            return RedirectToAction("Admin");
+//        }
+
+//        // ADMIN: View complaint details
+//        public IActionResult Details(int id)
+//        {
+//            var complaint = _service.GetComplaintById(id);
+//            if (complaint == null)
+//                return NotFound();
+
+//            return View("~/Views/Complaints/Details.cshtml", complaint);
+//        }
+
+//        // STUDENT: View submit form (GET)
+//        [HttpGet]
+//        public IActionResult Submit()
+//        {
+//            return View("~/Views/Complaints/Submit.cshtml");
+//        }
+
+//        // STUDENT: Submit complaint (POST)
+//        [HttpPost]
+//        [ValidateAntiForgeryToken]
+//        public IActionResult Submit(Complaint complaint)
+//        {
+//            _service.AddComplaint(complaint); // Time & Status set inside service
+//            TempData["Success"] = "Complaint submitted successfully!";
+//            return RedirectToAction("Index");
+//        }
+//    }
+//}
+using HostelManagementSystem.Models;
 using HostelManagementSystem.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,20 +101,24 @@ namespace HostelManagementSystem.Controllers
             _service = service;
         }
 
-        
+        // 🔹 STUDENT: View ONLY their complaints
         public IActionResult Index()
         {
-            var complaints = _service.GetAllComplaints();
+            string studentName = HttpContext.Session.GetString("Username") ?? string.Empty;
+
+            var complaints = _service.GetComplaintsByStudent(studentName);
+
             return View("~/Views/Complaints/Index.cshtml", complaints);
         }
 
+        // 🔹 ADMIN: View ALL complaints
         public IActionResult Admin()
         {
             var complaints = _service.GetAllComplaints();
             return View("~/Views/Complaints/Admin.cshtml", complaints);
         }
 
-        // ADMIN: Update status to "Received"
+        // 🔹 ADMIN: Update status
         [HttpPost]
         public IActionResult MarkReceived(int id)
         {
@@ -34,7 +126,6 @@ namespace HostelManagementSystem.Controllers
             return RedirectToAction("Admin");
         }
 
-        // ADMIN: Update status to "In Progress"
         [HttpPost]
         public IActionResult MarkInProgress(int id)
         {
@@ -42,7 +133,6 @@ namespace HostelManagementSystem.Controllers
             return RedirectToAction("Admin");
         }
 
-        // ADMIN: Update status to "Done"
         [HttpPost]
         public IActionResult MarkDone(int id)
         {
@@ -50,7 +140,7 @@ namespace HostelManagementSystem.Controllers
             return RedirectToAction("Admin");
         }
 
-        // ADMIN: Delete complaint
+        // 🔹 ADMIN: Delete complaint
         [HttpPost]
         public IActionResult Delete(int id)
         {
@@ -58,7 +148,7 @@ namespace HostelManagementSystem.Controllers
             return RedirectToAction("Admin");
         }
 
-        // ADMIN: View complaint details
+        // 🔹 ADMIN: View complaint details
         public IActionResult Details(int id)
         {
             var complaint = _service.GetComplaintById(id);
@@ -68,19 +158,24 @@ namespace HostelManagementSystem.Controllers
             return View("~/Views/Complaints/Details.cshtml", complaint);
         }
 
-        // STUDENT: View submit form (GET)
+        // 🔹 STUDENT: Show submit form
         [HttpGet]
         public IActionResult Submit()
         {
             return View("~/Views/Complaints/Submit.cshtml");
         }
 
-        // STUDENT: Submit complaint (POST)
+        // 🔹 STUDENT: Submit complaint
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Submit(Complaint complaint)
         {
-            _service.AddComplaint(complaint); // Time & Status set inside service
+            string studentName = HttpContext.Session.GetString("Username") ?? string.Empty;
+
+            complaint.StudentName = studentName;
+
+            _service.AddComplaint(complaint);
+
             TempData["Success"] = "Complaint submitted successfully!";
             return RedirectToAction("Index");
         }
