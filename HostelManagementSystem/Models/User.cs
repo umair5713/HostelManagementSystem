0bacc97bacb68d891465a89f8ad35ac40d35bbe7
Umair Atique <umairatique6346@gmail.com>
2026-05-03 19:23:21 +0500

    all done

--- HostelManagementSystem/Models/User.cs
+++ HostelManagementSystem/Models/User.cs
@@ -1,25 +1,4 @@
-﻿//using System.ComponentModel.DataAnnotations.Schema;
-
-//namespace HostelManagementSystem.Models
-//{
-//   [Table("tbl_user")]
-//   public class User
-//   {
-//        [Column("id_user")]
-//        public int Id { get; set; }
-//        [Column("email")]
-//        public string Email { get; set; } = string.Empty;
-//        [Column("user_password")]
-//        public string Password { get; set; } = string.Empty;
-//        [Column("fk_role_name")]
-
-//        public string FkRoleName { get; set; } = string.Empty;
-
-//        public int? FkStudentId { get; set; }
-//    }
-
-//}
-
+﻿
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -41,8 +20,6 @@ namespace HostelManagementSystem.Models
         [Column("fk_role_name")]
         public string FkRoleName { get; set; } = string.Empty;
 
-        [Column("fk_student_id")]
-        public int? FkStudentId { get; set; }
     }
 }
 
